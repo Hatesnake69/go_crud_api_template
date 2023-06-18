4e578c38219a3d0972a43c91f11c8982ac31edce
Hatesnake69 <vladimir@itsy.dev>
2023-06-19 00:09:37 +0300

    add swagger

--- endpoints/hello_world.go
+++ endpoints/hello_world.go
@@ -4,14 +4,25 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-func helloWorldHandler(c *gin.Context) {
+// @Summary Hello World
+// @Description Get a hello message
+// @ID get-hello-world
+// @Produce  json
+// @Success 200 {object} string "OK"
+// @Router /hello [get]
+func HelloWorldHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Hello, World!",
 	})
 }
 
 func SetupRoutes(router *gin.Engine) {
-	router.GET("/", helloWorldHandler)
+	router.GET("/hello", HelloWorldHandler)
+
+	swaggerURL := ginSwagger.URL("/swagger/doc.json")
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, swaggerURL))
 }
--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"go_crud_api_template/docs"
 	"go_crud_api_template/endpoints"
 
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func main() {
+	docs.SwaggerInfo.Title = "Your API Title"
+	docs.SwaggerInfo.Description = "Your API Description"
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = "localhost:8080"
+	docs.SwaggerInfo.BasePath = "/"
+
 	router := gin.Default()
-	endpoints.SetupRoutes(router)
+
+	// Register the Swagger handler
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, ginSwagger.URL("/swagger/doc.json")))
+
+	// Use a different path for your hello world endpoint
+	router.GET("/hello", endpoints.HelloWorldHandler)
+
 	router.Run(":8080")
 }
